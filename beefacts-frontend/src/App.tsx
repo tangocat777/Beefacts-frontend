da671911c176e15cedd0197b817e3d3bf5638ff5
mitchellmartin776 <mitchell.martin776@gmail.com>
2025-02-02 18:30:34 -0500

    Add vite, react, typescript, starting html and css

---
+++ beefacts-frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-row min-h-screen justify-center items-center'>
+          <section className="text-gray-600 body-font">
+      <div className="max-w-5xl pt-52 pb-24 mx-auto">
+        <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
+          Create your next website fast and easy
+        </h1>
+        <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
+          nine4 is a free to use template website for websites made
+          <br />
+          with Next.js and styled with Tailwind CSS
+        </h2>
+        <div className="ml-6 text-center">
+          <a
+            className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
+            href="/"
+          >
+            <div className="flex text-lg">
+              <span className="justify-center">View All Templates</span>
+            </div>
+          </a>
+          <a
+            className="inline-flex items-center py-3 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent ml-11 bg-gradient-to-r from-blue-500 to-blue-800 px-14 text-md md:mt-0 focus:shadow-outline"
+            href="/"
+          >
+            <div className="flex text-lg">
+              <span className="justify-center">Purchase</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <img
+          className="object-cover object-center w-3/4 mb-10 border shadow-md g327"
+          alt="Placeholder Image"
+          src="./images/placeholder.png"
+        ></img>
+      </div>
+      </section>
+  </div>
+    </>
+  )
+}
+
+export default App
