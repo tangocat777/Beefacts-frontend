8cb97d7570fda3265bdf53d07b281c98ddd3b172
mitchellmartin776 <mitchell.martin776@gmail.com>
2025-02-03 17:26:10 -0500

    Replacing template with components

---
+++ beefacts-frontend/src/BeeFactContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function BeeFactContainer() {
+    return (     
+    <div className="ml-6 text-center">
+        <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray text-center">
+          Click here to get your free bee fact.
+        </h2>
+     <a
+      className="inline-flex items-center py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-transparent bg-white px-7 text-md md:mt-0 hover:text-black hover:bg-white focus:shadow-outline"
+      href="/"
+    >
+      <div className="flex text-lg">
+        <span className="justify-center">Click here</span>
+      </div>
+    </a>
+  </div>
+  )}
+
+export default BeeFactContainer;
